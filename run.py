9f532b29a01337764995904903de4714675fd630
Stephenkofipoku <>
2023-10-03 21:04:44 +0000

    Add Wire up APIs and confirm access to google sheets

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Set up credentials and authorize the Google Sheets API
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('weatherpredictor')
+
+# Select the desired sheet within the spreadsheet
+weatherhistory = SHEET.worksheet('weatherhistory')
+
+# Read the data from the sheet into a Pandas DataFrame
+data = weatherhistory.get_all_records()
+
+print(data)
