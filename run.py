098d004717affa8e499578fdd9015bb0abf008f9
Stephenkofipoku <>
2023-10-05 12:35:34 +0000

    Adjust to method to authorize google sheets

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import base64
 from io import BytesIO
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 
 # Set up credentials and authorize the Google Sheets API
 
@@ -14,10 +16,12 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('weatherpredictor')
+def authorize_google_sheets():
+    """Authorize and return the Google Sheets client."""
+    creds = Credentials.from_service_account_file('creds.json')
+    scoped_creds = creds.with_scopes(SCOPE)
+    gspread_client = gspread.authorize(scoped_creds)
+    return gspread_client
 
 # Select the desired sheet within the spreadsheet
 weatherhistory = SHEET.worksheet('weatherhistory')
@@ -40,7 +44,7 @@ df['Precip Type'] = df['Precip Type'].astype('category')
 columns_to_drop = ['Loud Cover', 'Daily Summary']
 df = df.drop(columns_to_drop, axis=1)
 
-# Exporatory Data Analysis (EDA) on the dataset and gain insights into the patterns and relationships between variables.
+# Exporatory Data Analysis (EDA) on the dataset and gain insights into the dataset and relationships between variables.
 # Using libraries Matplotlib and Seaborn to create visualizations.
 # Visualize the average temperature by month
 
@@ -50,19 +54,22 @@ sns.histplot(data=df, x='Temperature (C)', bins=30)
 plt.title('Temperature Distribution')
 plt.xlabel('Temperature (C)')
 plt.ylabel('Count')
-plt.show()
-
-# Convert the plot to a base64-encoded image
-buffer = BytesIO()
-plt.savefig(buffer, format='png')
-buffer.seek(0)
-image_base64 = base64.b64encode(buffer.getvalue()).decode()
-
-# Insert the image into the Google Spreadsheet
-analyzed_sheet = SHEET.worksheet('analyzed')
-analyzed_sheet.update([[f'=IMAGE("data:image/png;base64,{image_base64}", 1)']], 'A1')
-
+plt.savefig('temperature_distribution.png')
 plt.close()
+
+# Upload the temperature distribution image to Google Drive
+drive_service = build('drive', 'v3', credentials=SCOPED_CREDS)
+file_metadata = {
+    'name': 'temperature_distribution.png',
+    'parents': [SHEET.id]
+}
+media = MediaFileUpload('temperature_distribution.png', mimetype='image/png')
+uploaded_file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+image_url = f"https://drive.google.com/uc?id={uploaded_file['id']}"
+
+# Insert the temperature distribution image into the Google Spreadsheet
+analyzed_sheet = SHEET.worksheet('analyzed')
+analyzed_sheet.update('A1', [[f'=IMAGE("{image_url}", 1)']])
 
 # Visualize the relationship between temperature and humidity
 plt.figure(figsize=(8, 6))
@@ -70,10 +77,30 @@ sns.scatterplot(data=df, x='Temperature (C)', y='Humidity')
 plt.title('Temperature vs Humidity')
 plt.xlabel('Temperature (C)')
 plt.ylabel('Humidity')
+plt.savefig('temperature_vs_humidity.png')
+plt.close()
+
+# Upload the temperature vs. humidity image to Google Drive
+file_metadata = {
+    'name': 'temperature_vs_humidity.png',
+    'parents': [SHEET.id]
+}
+media = MediaFileUpload('temperature_vs_humidity.png', mimetype='image/png')
+uploaded_file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+image_url = f"https://drive.google.com/uc?id={uploaded_file['id']}"
+
+# Insert the temperature vs. humidity image into the Google Spreadsheet
+analyzed_sheet.update('D1', [[f'=IMAGE("{image_url}", 1)']])
+
+# Print the plots in the terminal
+sns.histplot(data=df, x='Temperature (C)', bins=30)
+plt.title('Temperature Distribution')
+plt.xlabel('Temperature (C)')
+plt.ylabel('Count')
 plt.show()
 
-# Convert the plot to a base64-encoded image
-buffer = BytesIO()
-plt.savefig(buffer, format='png')
-buffer.seek(0)
-image_base64 = base64.b64encode(buffer.getvalue()).decode()
+sns.scatterplot(data=df, x='Temperature (C)', y='Humidity')
+plt.title('Temperature vs Humidity')
+plt.xlabel('Temperature (C)')
+plt.ylabel('Humidity')
+plt.show()
